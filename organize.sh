1ba5c9cd52d9a86ca45d09429f561dd5b78c6302
Schuyler Martin <smartin@UNSCFedora.home>
2014-06-08 14:32:32 -0400

    Unzipping and managing top-level zip is working
    Creating a file structure for the top-level zip (the one provided from
    my courses) works as well as unzipping the files into the structure
    
    This directory structure is made local to the scripts execution.
    Currently I have a sym link set in my bin to point to this script,
        which works well for me
    
    In addition:
    - Script can handle zip's in another path (like an absolute)
    - New flags added, included a flag to delete the zips after extraction

--- organize.sh
+++ organize.sh
@@ -11,8 +11,14 @@
 ####  CONSTANTS  ####
 #Mark of failure
 MRKFAIL="FAIL_"
+#Usage message string
+USAGE="Usage: ./organize.sh [-c] [-q] file.zip [file(s) ...]"
 
 ####    FLAGS    ####
+#quiet mode; 0 for on
+QUIET=1
+#cleans up top-level zip automatically; 0 for on 
+CLEANUP=1
 
 #### GLOBAL VARS ####
 #List of zip files passed in
@@ -24,32 +30,25 @@ pathList=()
 
 #Functions for wrapping echo for color and STDERR
 function echoerr {
-    if [[ ! ${CRON} = ${ON} ]]; then
+    if [[ ! ${QUIET} = 0 ]]; then
         tput setaf 1
-    fi
-    echo "ERROR: "${@} 1>&2 
-    if [[ ! ${CRON} = ${ON} ]]; then
+        echo "ERROR: "${@} 1>&2 
         tput sgr0
     fi
 }
 
 function echowarn {
-    if [[ ! ${CRON} = ${ON} ]]; then
+    if [[ ! ${QUIET} = 0 ]]; then
         tput setaf 3
-    fi
-    echo "WARNING: "${@}
-    if [[ ! ${CRON} = ${ON} ]]; then
+        echo "WARNING: "${@}
         tput sgr0
     fi
 }
 
 function echosucc {
-
-    if [[ ! ${CRON} = ${ON} ]]; then
+    if [[ ! ${QUIET} = 0 ]]; then
         tput setaf 2
-    fi
-    echo ${@}
-    if [[ ! ${CRON} = ${ON} ]]; then
+        echo ${@}
         tput sgr0
     fi
 }
@@ -57,15 +56,14 @@ function echosucc {
 #Takes the zip files and makes the appropriate directory structure
 #@param: 
 #        $1..$N top-level zip files to organize. Each one will be put into
-#           a lab folder, then a section.
+#           a lab folder, then a section. This can be a path to a file.
 #           Example: "Lab 10 Download Jun 8, 2014 1110 AM.zip" will go into 
 #           lab10/sec_a and then the next file passed in will become sec_b, etc
 #
 #@return: 
-#        - newFolderName the new, sanitized folder name
-#
+#        - failCntr return the number of failures occured (acts as $?)
 #@global:
-#
+#        - pathList is set with the local pathings to extracted file structures
 function mkZipDirs {
     #starting code to count up letters
     local asciiCode=97
@@ -75,28 +73,48 @@ function mkZipDirs {
     #local naming vars
     local secChar=""
     local labNum=0
+    local zip=""
+    #counters
     local i=0
-    for zip in "${@}"; do
+    failCntr=0
+    for zipPath in "${@}"; do
+        #extract just the zip name from a possible directory
+        zip=$(basename "${zipPath}")
         labNum=$(echo ${zip} | grep -o -e "Lab [0-9][0-9]*" | sed 's/Lab //')
         secChar=$(printf "\x$(printf %x ${asciiCode})")
         pathList[$i]="${LABNAME}${labNum}/${SECNAME}${secChar}"
-        echo mkdir -p "${pathList[$i]}" 
+        mkdir -p "${pathList[$i]}" 
         #if folder creation is successful, continue with unpacking the zips
         if [[ $? = 0 ]]; then
             #now unzip the file into the newly created folder
-             
-            if [[ ! $? = 0 ]]; then
+            unzip -q "${zipPath}" -d ${pathList[$i]} 
+            if [[ $? = 0 ]]; then
+                #if cleanup mode is enabled, remove the original zip
+                if [[ ${CLEANUP} = 0 ]]; then
+                    rm "${zipPath}"
+                    if [[ ! $? = 0 ]]; then
+                        echoerr "Clean up mode: failed to delete ${zipPath}"
+                    fi
+                fi
+            else
                 echoerr "Failed to unpack ${zip}"
                 #mark failures to keep list in alignment and for future checks
                 pathList[$i]=${MRKFAIL}"${pathList[$i]}"
+                let failCntr++
             fi
         else
             echoerr "Failed create folder for ${zip}"
             pathList[$i]=${MRKFAIL}"${pathList[$i]}"
+            let failCntr++
         fi
         let asciiCode++
         let i++
     done
+    if [[ ${failCntr} = 0 ]]; then
+        echosucc "Top-level zip files successfully unpacked!"
+    else
+        echoerr "${failCntr} failures unpacking the top-level zip files"
+    fi
 }
 
 #Parses the file name into the proper file name
@@ -145,22 +163,34 @@ function name {
 }
 
 ####   GETOPTS   ####
-#May include flags/modes of operation later
-#while getopts ":v" opt; do
-#  case $opt in
-#    v)
-#      
-#      ;;
-#    *)
-#      
-#      ;;
-#  esac
-#done
+#Flags for modes of operation
+while getopts ":cq" opt; do
+    case $opt in
+        c)
+            CLEANUP=0
+            ;;
+        q)
+            QUIET=0
+            ;;
+        *)
+            echo "${USAGE}"
+            exit 1
+            ;;
+    esac
+done
 
 ####    MAIN     ####
 function main {
+    #shift after reading getopts
+    shift $(($OPTIND - 1))
     #record list of file names from command line args
     zipList=("${@}")
+    #no args after flags, present usage message
+    if [[ ${#zipList[@]} = 0 ]]; then
+        echo "${USAGE}"
+        exit 1
+    fi
+    #turn the zips into a local file structure
     mkZipDirs "${zipList[@]}"
 }
 
