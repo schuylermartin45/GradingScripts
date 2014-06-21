dff9a84823f89f212323c45b94a0e3bde451241d
schuylermartin45 <schuylermartin45@gmail.com>
2014-06-21 16:31:28 -0400

    Lots of changes to batchRun.sh
    Most of the functionality is in place. Does not actually run tests
    arguments for python yet. Java program automation will come later.

--- batchRun.sh
+++ batchRun.sh
@@ -23,25 +23,39 @@ DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
 source ${DIR}"/.commonLib.sh"
 
 ####  CONSTANTS  ####
-USAGE="Usage: ./runBatch.sh [-q] path_to_lab exec_file"
+USAGE="Usage: ./runBatch.sh [-q] [-t] [time_out] #_tests lab exec_file"
 
 #file extensions for comparison purposes; does not include dot for pattern 
 #   purposes (issue with escaping the character in a variable)
 EXT_JAVA="java"
 EXT_PY="py"
 
+#Default timeout given to a program's run time
+TIME_OUT="60s"
+
 ####    FLAGS    ####
 #All flags are = 0 for on
+
+#User-specified timeout for the program runs
+TIMEFLAG=1
 
 #### GLOBAL VARS ####
 
 #values passed in
+#number of tests to run
+numTests=0
 #path to lab
 labDIR=""
+#file that gets run
 execFile=""
 
 #determined file type for this lab
 fileType=""
+
+#array of test arguments; one index for test
+testArgs=()
+#expected output files for each test; these are the files we diff against
+expectedOut=()
 
 ####  FUNCTIONS  ####
 
@@ -89,6 +103,49 @@ function determineFileType {
     fi
 }
 
+#Asks for the arguments to pass to each program for each test and stores them
+#as a list of test arguments; for simplicity files 
+#@param: 
+#
+#@return: 
+#
+#@global:
+#        - testArgs List of test arguments that is set
+function testArgMenu {
+    echosucc "==== Setting Test Arguments ===="
+    #display all files in the test files folder, for user's convenience
+    if [[ -z $(ls ${labDIR}/${TEST_DIR}) ]]; then
+        #warn that test files are missing
+        echowarn "No files were found in the test directory."
+    else
+        echo "Here are the files found in the test folder:"
+        for file in "${labDIR}/${TEST_DIR}"/*; do
+            echo "    $(basename ${file})"
+        done
+    fi
+    #read in the arguments
+    local i=0
+    local args=""
+    local exOutFile=""
+    while [[ $i -lt ${numTests} ]]; do
+        #do not check input(s) for empty strings, in case a program doesn't
+        #need arguments or have expected output files to test against
+        read -p "Enter args for test[$i]: " args
+        testArgs[$i]="${args}"
+        read -p "Enter an expected output file for test[$i]: " exOutFile
+        expectedOut[$i]="${exOutFile}"
+        let i++
+    done
+    echo Args:
+    for item in "${testArgs[@]}"; do
+        echo $item
+    done
+    echo ExpectedOutput:
+    for item in "${expectedOut[@]}"; do
+        echo $item
+    done
+}
+
 #Copy all provided files into a student's directory
 #   This will not stomp over any files with the same name; if students submit
 #   modified files they were not supposed to change, their programs will fail 
@@ -115,27 +172,45 @@ function cpProvidedFiles {
 #Run a Python program
 #@param:
 #        $1 Path to student's directory
+#        $2 Test number
 #@return:
 #
 #@global:
 #
 function runPy { 
     local stuDIR="$1"
-    python3 "${stuDIR}"
+    local testNum=$2
+    #location to store output
+    local outFile="${stuDIR}${OUTPUT_DIR}/${OUT_FILE}$i"
+    #push test arguments in and record all output
+    timeout ${TIME_OUT} python3 "${stuDIR}${execFile}" &> "${outFile}"
+    #if the program timed-out, it'll be recorded in $? as an error code
+    if [[ ! $? = 0 ]]; then
+        echo "Program timed-out with error code: $?" >> "${outFile}"
+    fi
 }
 
 #Run a Java program
 #@param:
 #        $1 Path to student's directory
+#        $2 Test number
 #@return:
 #
 #@global:
 #
 function runJava {
     local stuDIR="$1"
+    local testNum=$2
+    #TODO: handle compiling all files in the student dir based on dependency
+    #might need to break args up by spaces
+    timeout ${TIME_OUT} java "${stuDIR}${execFile}" ${testArgs[$testNum]}
+    #if the program timed-out, it'll be recorded in $? as an error code
+    if [[ ! $? = 0 ]]; then
+        echo "Program timed-out with error code: $?" >> "${outFile}"
+    fi
 }
 
-#Hanlde running either kind of program
+#Handle running either kind of program; dumping all the output to a file
 #@param:
 #        $1 Path to student's directory
 #@return:
@@ -144,11 +219,26 @@ function runJava {
 #        - fileType type of lab this is
 function runProgram {
     local stuDIR="$1"
-    if [[ ${fileType} = ${EXT_PY} ]]; then
-        runPy "${stuDIR}"
-    else
-        runJava "${stuDIR}"
-    fi
+    local i=0
+    local outFile=""
+    local diffFile=""
+    local exOutFile=""
+    #looping structure for all tests provided by the user
+    while [[ $i -lt ${numTests} ]]; do
+        if [[ ${fileType} = ${EXT_PY} ]]; then
+            runPy "${stuDIR}" $i
+        else
+            runJava "${stuDIR}" $i
+        fi
+        #run diff of the output after the run, if applicable
+        if [[ ! -z ${expectedOut[$i]} ]]; then
+            outFile="${stuDIR}${OUTPUT_DIR}/${OUT_FILE}$i"
+            diffFile="${stuDIR}${OUTPUT_DIR}/${DIFF_FILE}$i"
+            exOutFile="${labDIR}/${EXPECTED_DIR}/${expectedOut[$i]}"
+            diff "${outFile}" "${exOutFile}" &> "${diffFile}"
+        fi
+        let i++
+    done
 }
 
 ####   GETOPTS   ####
@@ -156,6 +246,9 @@ while getopts ":q" opt; do
     case $opt in
         q)
             QUIET=0
+            ;;
+        t)
+            TIMEFLAG=0
             ;;
         *)
             echo ${USAGE} 
@@ -168,15 +261,24 @@ done
 function main {
     #shift after reading getopts
     shift $(($OPTIND - 1))
+    #if the time out is specified, it becomes the first arg
+    if [[ ${TIMEFLAG} = 0]]; then
+        TIME_OUT="$1"
+        shift 1
+    fi
     #no args after flags, present usage message
-    if [[ ${#@} -le 1 ]]; then
+    if [[ ${#@} -lt 3 ]]; then
         echo ${USAGE}
         exit 1
     fi
-    labDIR=$1
-    execFile=$2
+    #set args passed in
+    numTests=$1
+    labDIR="$2"
+    execFile="$3"
     #check what kind of lab this is
     determineFileType
+    #define arguments passed in for each test
+    testArgMenu
     #loop over all sections in a lab; only folders
     for sec in "${labDIR}/${SECNAME}"*/; do
         #loop over all students in a section; only folders
